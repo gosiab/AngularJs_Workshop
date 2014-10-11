fe3ec3ab3bf25a9dbe0802c3f432c1e13a4b9586
Gosia Borzęcka <gosia.borzecka@gmail.com>
2014-10-11 20:03:14 +0200

    Second iteration

--- main.js
+++ main.js
@@ -1,22 +1,14 @@
-function MyFirstCtrl($scope){
-  
-  $scope.sorting = {
-        by: 'price',
-        desc: true
-    };
-    
-    $scope.sortBy = function(byWhat) {
-        $scope.sorting.by = byWhat; 
-        $scope.sorting.desc = !$scope.sorting.desc;
-    }
-  
+var app = angular.module("ShopApp", []);
+
+
+app.controller("MyFirstCtrl", function ($scope) {
   $scope.kafeleks = [
     {
       product: 'http://lorempixel.com/output/city-q-c-177-102-4.jpg',
       name: 'City',
       shortDescription: 'City description',
       description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software..',
-      price: 10
+      price: 10,
     },
     {
       product: 'http://lorempixel.com/output/food-q-c-177-102-4.jpg',
@@ -38,5 +30,29 @@ function MyFirstCtrl($scope){
       shortDescription: 'Transport description',
       description: ' Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software..',
       price: 200
-    }]
-}
+    }];
+    
+    $scope.sorting = {
+      by: 'price',
+      desc: true
+    };
+});
+
+app.directive('sortingButton', function(){
+    
+    return {
+        restrict: 'E',
+        scope: {
+            text: '@',
+            value: '@',
+            sort: '='
+        },
+        templateUrl: 'sortingButton.html',
+        link: function(scope) {
+            scope.sortBy = function(byWhat) {
+                scope.sort.by = byWhat; 
+                scope.sort.desc = !scope.sort.desc;
+            };
+        }
+    };
+});
